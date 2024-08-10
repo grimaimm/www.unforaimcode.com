9f16780a446d8e3cd2b8c0dd1626cb90e2763f47
grimaim <muhammadrahim.mr196@gmail.com>
2024-08-10 14:55:10 +0700

    fix: dashboard page

--- src/modules/dashboardPage/components/Dashboard.jsx
+++ src/modules/dashboardPage/components/Dashboard.jsx
@@ -12,19 +12,19 @@ const Dashboard = () => {
       <PageBreakline className='mb-8 mt-10' />
       <CodingActive />
       <PageBreakline className='mb-8 mt-10' />
-      <div className='space-y-10'>
-        {GITHUB_ACCOUNTS?.filter((account) => account?.is_active).map(
-          (account, index) => (
-            <Contributions
-              key={index}
-              username={account?.username}
-              type={account?.type}
-              endpoint={account?.endpoint}
-            />
-          ),
-        )}
-      </div>
-      <PageBreakline className='mb-8 mt-10' />
+      {/* <div className='space-y-10'> */}
+      {GITHUB_ACCOUNTS?.filter((account) => account?.is_active).map(
+        (account, index) => (
+          <Contributions
+            key={index}
+            username={account?.username}
+            type={account?.type}
+            endpoint={account?.endpoint}
+          />
+        ),
+      )}
+      {/* </div> */}
+      {/* <PageBreakline className='mb-8 mt-10' /> */}
     </>
   );
 };
