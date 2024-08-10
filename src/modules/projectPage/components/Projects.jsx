2f32b8b83fc7116aa0cefb15d541b9a0eccb68b1
grimaim <muhammadrahim.mr196@gmail.com>
2024-08-10 17:00:28 +0700

    fix: initial animation

--- src/modules/projectPage/components/Projects.jsx
+++ src/modules/projectPage/components/Projects.jsx
@@ -37,9 +37,9 @@ const Projects = ({ projects, loadMore, hasMore }) => {
         {filteredProjects.map((project, index) => (
           <motion.div
             key={project.id || index}
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: index * 0.1 }}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
           >
             <ProjectCard project={project} />
           </motion.div>
