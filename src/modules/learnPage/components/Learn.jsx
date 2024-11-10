150437c6a93c1543d26d521c9fdcb7cbe9b8c341
grimaim <muhammadrahim.mr196@gmail.com>
2024-11-11 00:04:58 +0700

    chore: add filtered in learns content

--- src/modules/learnPage/components/Learn.jsx
+++ src/modules/learnPage/components/Learn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import LearnCard from './LearnCard';
 import { FetchLearns } from '@/pages/api/learn';
@@ -9,13 +9,19 @@ export async function getServerSideProps() {
 }
 
 const Learn = ({ learns }) => {
-  if (!Array.isArray(learns)) {
+  const [filteredLearns, setFilteredLearns] = useState([]);
+
+  useEffect(() => {
+    setFilteredLearns(learns.filter((learn) => learn.is_show));
+  }, [learns]);
+
+  if (filteredLearns.length === 0) {
     return <div>No learns available.</div>;
   }
 
   return (
     <div className='grid gap-5 pb-5 pt-2 sm:grid-cols-2 lg:px-2'>
-      {learns.map((learn, index) => (
+      {filteredLearns.map((learn, index) => (
         <motion.div key={index}>
           <LearnCard learn={learn} />
         </motion.div>
