4e9ab9940d3e43f97534a8f71286b76c3c86b274
grimaim <muhammadrahim.mr196@gmail.com>
2025-01-30 02:52:35 +0700

    chore: update navbar and layout

--- src/common/components/layouts/LayoutWithoutSidebar.jsx
+++ src/common/components/layouts/LayoutWithoutSidebar.jsx
@@ -6,6 +6,7 @@ const LayoutWithoutSidebar = ({ children }) => {
   return (
     <>
       <div className='lg:dark:bg-dark-wrapper--nosidebar'></div>
+      <div className='absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#80808018_1px,transparent_1px),linear-gradient(to_bottom,#80808018_1px,transparent_1px)] bg-[size:30px_30px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_0%,#000_80%,transparent_100%)]'></div>
       <div className='layout flex flex-col items-stretch'>
         <header className='header'>
           <Navbar />
--- src/common/components/partials/navbar/components/NavbarLayout.jsx
+++ src/common/components/partials/navbar/components/NavbarLayout.jsx
@@ -1,15 +1,36 @@
 import * as React from 'react';
 
 const NavbarLayout = ({ children, isOpen }) => {
+  const [hasBorder, setHasBorder] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setHasBorder(true);
+      } else {
+        setHasBorder(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className='block lg:hidden'>
       <div className='w-full'>
         <div
-          className={`navbar-layout fixed top-0 z-10 flex w-full flex-col justify-between border-b border-zinc-200 px-2 text-zinc-700 dark:border-zinc-800 dark:text-zinc-200 ${
+          className={`navbar-layout fixed top-0 z-10 flex w-full flex-col justify-between px-2 text-zinc-700 transition-[border] duration-300 dark:text-zinc-200 ${
+            hasBorder
+              ? 'border-b border-zinc-200 dark:border-zinc-800'
+              : 'border-b border-transparent'
+          } ${
             isOpen
               ? 'bg-zinc-50 backdrop-blur-none dark:bg-zinc-950'
-              : 'bg-zinc-50/80 backdrop-blur-md dark:bg-zinc-950/80'
-          } `}
+              : 'bg-zinc-50/70 backdrop-blur-md dark:bg-zinc-950/70'
+          }`}
         >
           {children}
         </div>
